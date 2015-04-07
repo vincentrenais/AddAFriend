9c768f8684a1895a3b9c7251919461455dded7f0
Vincent Renais <vincentrenais@gmail.com>
2015-04-07 14:55:48 -0400

    Initial Commit

---
+++ AddAFriend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddAFriend
+//
+//  Created by Vincent Renais on 2015-04-07.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddAFriend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddAFriend
+//
+//  Created by Vincent Renais on 2015-04-07.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
